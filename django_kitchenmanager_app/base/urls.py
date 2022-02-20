f5ced34d4d74d94bf960271e0ed606481331fa29
tomek <tomeklorenz@gmail.com>
2022-02-20 18:51:54 +0100

    setting up bootstrap template

--- django_kitchenmanager_app/base/urls.py
+++ django_kitchenmanager_app/base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.food_analysis_dashboard, name="food-analysis-dashboard"),
+    path('website/', views.index, name="index")
+    # path('/costs', views)
 ]
--- django_kitchenmanager_app/base/views.py
+++ django_kitchenmanager_app/base/views.py
@@ -1,10 +1,48 @@
+from django.forms import fields_for_model
 from django.shortcuts import render
 from .models import Profile, FoodProduct, FoodGroup, FoodPrice, NutritionalValue
 from django.contrib.auth.models import User
+import datetime
 # Create your views here.
 
-def home(request):
+def index(request):
+    context = {}
+    return render(request, 'base/index.html', context)
+
+def food_analysis_dashboard(request):
     food = FoodProduct.objects.all()
+    context = {"food": food}
+    return render(request, 'base/food_analysis.html', context)
+
+def cost_analysis_dashboard(request):
     prices = FoodPrice.objects.all()
-    context = {"food": food, "prices": prices}
-    return render(request, 'base/home.html', context)
+    context = {"prices": prices}
+    return render(request, 'base/cost_analysis.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'base/profile.html', context)
+
+def add_food_product(request):
+    context = {}
+    return render(request, 'base/food_product_form.html', context)
+
+def edit_food_product(request):
+    context = {}
+    return render(request, 'base/food_product_form.html', context)
+
+def delete_food_product(request):
+    context = {}
+    return render(request, 'base/food_product_form.html', context)
+
+def add_profile(request):
+    context = {}
+    return render(request, 'base/profile_form.html', context)
+
+def edit_profile(request):
+    context = {}
+    return render(request, 'base/profile_form.html', context)
+
+def delete_profile(request):
+    context = {}
+    return render(request, 'base/profile_form.html', context)
